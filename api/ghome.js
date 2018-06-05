3b33958f4124ade64b44be2bf4f336cb6dc17872
Thomas Detry <thom.detry@gmail.com>
2018-06-05 14:26:58 +0000

    last udpated - html to be cleaned

--- api/ghome.js
+++ api/ghome.js
@@ -14,25 +14,25 @@ const sagemakerruntime = new AWS.SageMakerRuntime({region:'us-east-2'})
 
 let params = {
   Body: '32',
-  EndpointName: 'kmeans-2018-05-18-08-39-13-156',
+  EndpointName: 'kmeans-2018-05-18-11-24-44-529',
   ContentType: 'text/csv'
 };
 
 // intents named matched in Dialogflow
-app.intent('0_test', conv => {
+app.intent('3_location', conv => {
   //console.log(conv)
-  conv.ask('Here is the list of the closest shops to your location')
-  conv.ask(`Would you like me to send the itinerary of the closest shop to your phone?`)
+  conv.ask('This is the closest place where this computer is still in stock')
+  conv.ask(`Would you like me to send the itinerary to your phone?`)
   socket.emit('location', 'this is a message from the backend')
 })
 
-app.intent('2_test', conv => {
-  conv.ask('Here is the list of the most popular products given your profile')
-  conv.ask(`Do you want me to check for promotion?`)
+app.intent('2_reco', conv => {
+  conv.ask('Given your profile, here are the bestsellers.')
+  conv.ask(`Do you like one of them?`)
   socket.emit('bestsellers', 'this is a message from the backend')
 })
 
-app.intent('cluster', conv => {
+app.intent('2_help_reco', conv => {
   sagemakerruntime.invokeEndpoint(params, (err, data) => {
     let results = 'test'  
     if (err) { console.log('AWS ' + err, err.stack) } // an error occurred
@@ -42,8 +42,8 @@ app.intent('cluster', conv => {
           socket.emit('cluster', results)
     }
   })
-    conv.ask('Here is your score')
-    conv.ask(`Do you want something else?`)
+    conv.ask('Here is my recommendation')
+    conv.ask(`Do you like it?`)
 })
 
 app.intent('joke', conv => {
